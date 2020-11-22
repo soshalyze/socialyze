d55bc3f624b8e443881e1c63e31b81a572a2deec
nyando <nicki.dorrmann@googlemail.com>
2020-11-22 10:25:22 +0100

    basic backend class structure

--- app/models.py
+++ app/models.py
@@ -2,16 +2,30 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class RedditPostManager(models.Manager):
+    def get_all_posts(self, user):
+        return super().get_queryset().filter(app_user=user)
+
+    def get_subreddit_posts(self, user, subname):
+        return self.get_all_posts(user).filter(subreddit=subname)
+
+
+class RedditCommentManager(models.Manager):
+    def get_all_comments(self, user):
+        return super().get_queryset().filter(app_user=user)
+
+    def get_subreddit_comments(self, user, subname):
+        return self.get_all_comments(user).filter(subreddit=subname)
+
+
 class RedditUser(models.Model):
     app_user = models.ForeignKey(User, on_delete=models.CASCADE)
     username = models.CharField()
 
-    def create(self, app_user, username):
-
 
 class RedditPost(models.Model):
     username = models.ForeignKey(RedditUser, on_delete=models.CASCADE)
-    date = models.DateTimeField()
+    date = models.BigIntegerField()
     title = models.CharField()
     is_text = models.BooleanField()
     subreddit = models.CharField()
@@ -20,7 +34,7 @@ class RedditPost(models.Model):
 
 class RedditComment(models.Model):
     username = models.ForeignKey(RedditUser, on_delete=models.CASCADE)
-    date = models.DateTimeField()
+    date = models.BigIntegerField()
     content = models.TextField()
     score = models.IntegerField()
     subreddit = models.CharField()
--- app/reddit.py
+++ app/reddit.py
@@ -1,16 +1,35 @@
 import praw
-from app.models import RedditUser
+from django.contrib.auth import authenticate
+from app.models import *
+
 
 class RedditCommunication:
-    def __init__(self, reddit_user):
+    def __init__(self, request, reddit_username):
+        self.app_user = request.user.username
         self.reddit = praw.Reddit(client_id='CLIENT_ID',
                                   client_secret='CLIENT_SECRET',
                                   username='USERNAME',
                                   password='PASSWORD',
-                                  user_agent='socialyze')
-        self.reddit_user = self.reddit.redditor(reddit_user)
+                                  user_agent='python:commentreader:v0.1 (by u/socialyze_user)')
+        self.reddit_user = self.reddit.redditor(reddit_username)
+        self.db_user = RedditUser(app_user=self.app_user, username=reddit_username)
+        self.db_user.save()
 
     def fetch_comments(self):
         for comment in self.reddit_user.comments.new(limit=100):
+            db_comment = RedditComment(username=self.db_user,
+                                       date=comment.created_utc,
+                                       content=comment.body,
+                                       score=comment.score,
+                                       subreddit=comment.subreddit)
+            db_comment.save()
 
-
+    def fetch_posts(self):
+        for post in self.reddit_user.submissions.new(limit=50):
+            db_post = RedditPost(username=self.db_user,
+                                 date=post.created_utc,
+                                 title=post.title,
+                                 is_text=post.is_self,
+                                 subreddit=post.subreddit,
+                                 score=post.score)
+            db_post.save()
