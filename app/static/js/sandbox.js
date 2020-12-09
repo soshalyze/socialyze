a51803d77f2ebba73713cfc7f7f14979d6698185
Chris <christianrenner6@gmail.com>
2020-12-09 12:56:49 +0100

    Working dropdown menu in app/sandbox

--- app/static/js/sandbox.js
+++ app/static/js/sandbox.js
@@ -1,2 +1,4 @@
-//write here js functions for Sandbox
-
+  document.addEventListener('DOMContentLoaded', function() {
+    var elements = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elements, true);
+  });
--- users/static/js/sandbox.js
+++
@@ -1,5 +0,0 @@
-//write here js functions for Sandbox
-  document.addEventListener('DOMContentLoaded', function() {
-    var elements = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elements, true);
-  });
